effbd49807901f48625ae13721919679c37bfa37
saam1md <saam1mdalam@gmail.com>
2021-04-12 23:40:45 -0400

    true vs false

---
+++ true-false.js
@@ -0,0 +1,11 @@
+// Falsy Statement : 0, "", undefined, null, NaN, false
+// Truthy Statement : '0', ' ', []
+
+const age = 10;
+
+if (age> 5) {
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
